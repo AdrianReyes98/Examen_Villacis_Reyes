13ebec3a71f4b4f430b934ff1eb7678adee44fb5
AdrianReyes98 <areyes3804@uta.edu.ec>
2022-05-30 18:14:36 -0500

    Commit Cambio Layout Primera Activity

--- app/src/main/java/fisei/reyespc/examen_villacis_reyes/MainActivityRV.java
+++ app/src/main/java/fisei/reyespc/examen_villacis_reyes/MainActivityRV.java
@@ -10,5 +10,6 @@ public class MainActivityRV extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_rv);
+        getSupportActionBar().hide();
     }
 }
